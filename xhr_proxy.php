fce68b0074d7435259a06976dab087cb771ce7bc
Marián Kyral <mkyral@email.cz>
2016-02-05 22:28:10 +0100

    xhr_proxy: small fixes
    * Sent json/utf-8 header
    * Do not encode data

--- xhr_proxy.php
+++ xhr_proxy.php
@@ -2,11 +2,13 @@
 $url = (isset($_GET['url'])) ? $_GET['url'] : false;
 if(!$url) exit;
 
+header('Content-Type: application/json; charset=utf-8');
+
 $referer = (isset($_SERVER['HTTP_REFERER'])) ? strtolower($_SERVER['HTTP_REFERER']) : false;
 $is_allowed = $referer && strpos($referer, strtolower($_SERVER['SERVER_NAME'])) !== false;
 
-$string = ($is_allowed) ? utf8_encode(file_get_contents($url)) : 'You are not allowed to use this proxy!';
-$json = json_encode($string);
+$json = ($is_allowed) ? utf8_encode(file_get_contents($url)) : 'You are not allowed to use this proxy!';
+
 $callback = (isset($_GET['callback'])) ? $_GET['callback'] : false;
 if($callback){
 	$jsonp = "$callback($json)";
