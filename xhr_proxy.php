a1cb739fa98e306692b067efa25f3c5608575988
zbycz <zbytovsky@gmail.com>
2016-02-06 09:40:39 +0100

    xhr_proxy: added cors header, allow all

--- xhr_proxy.php
+++ xhr_proxy.php
@@ -1,13 +1,20 @@
 <?php
+
+//from http://www.ajax-cross-origin.com/how.html#js-how-it-works
+
 $url = (isset($_GET['url'])) ? $_GET['url'] : false;
 if(!$url) exit;
 
-header('Content-Type: application/json; charset=utf-8');
-
-$referer = (isset($_SERVER['HTTP_REFERER'])) ? strtolower($_SERVER['HTTP_REFERER']) : false;
-$is_allowed = $referer && strpos($referer, strtolower($_SERVER['SERVER_NAME'])) !== false;
+//$referer = (isset($_SERVER['HTTP_REFERER'])) ? strtolower($_SERVER['HTTP_REFERER']) : false;
+//$is_allowed = $referer && strpos($referer, strtolower($_SERVER['SERVER_NAME'])) !== false;
+$is_allowed = true;
 
 $json = ($is_allowed) ? utf8_encode(file_get_contents($url)) : 'You are not allowed to use this proxy!';
+
+//TODO add only rawgit, osm.cz, localhosts, etc
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json; charset=utf-8');
+
 
 $callback = (isset($_GET['callback'])) ? $_GET['callback'] : false;
 if($callback){
