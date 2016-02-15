a29c79d47202dc445d8e378b59c8668191edf0c8
zbycz <zbytovsky@gmail.com>
2016-02-15 18:35:41 +0100

    layout: last modified stamp for static files

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -4,3 +4,6 @@ FileTemplate::extensionMethod('timeago', function ($that, $s) {
     return Helpers::timeAgoInWords($s);
 });
 
+FileTemplate::extensionMethod('modified', function ($that, $s) {
+    return $s . "?". dechex(filemtime(WWW_DIR.$s));
+});
